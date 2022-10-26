174c87f0a496f60a98c4f04ae7d2ae9dbe532b77
oliver halvey <oliverhalvey310@yahoo.com>
2022-10-26 07:27:58 -0700

    Initial Commit

---
+++ three letter word/three_letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  three_letter_wordApp.swift
+//  three letter word
+//
+//  Created by Oliver Halvey on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct three_letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
